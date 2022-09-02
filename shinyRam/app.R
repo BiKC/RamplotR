40f027109842b5c72a1ceeaa6e3e1b5f701f6b0c
CedricHermansBIT <cedric.hermans2@howest.be>
2022-09-02 16:34:26 +0200

    Big update, fully remove R plotly and switch to js. Added support for different background density matrixes. Fix colors and added a load bar

--- shinyRam/app.R
+++ shinyRam/app.R
@@ -25,226 +25,252 @@ library(RColorBrewer)
 library(plyr)
 
 colorSet <- RColorBrewer::brewer.pal(8, "Accent")
+previousPDB<-""
 
 #load bg image
 fig <- readRDS("static/bgfig.RDS")
-densMatgeneral <- readRDS("static/densMatGeneral")
+densMatGeneral <- readRDS("static/densMatGeneral")
+densMatGly <- readRDS("static/densMatGly")
+densMatprePro <- readRDS("static/densMatprePro")
+densMatPro <- readRDS("static/densMatPro")
+
+allAA<-c(
+  "ALA",
+  "ARG",
+  "ASN",
+  "ASP",
+  "CYS",
+  "GLU",
+  "GLN",
+  "GLY",
+  "HIS",
+  "ILE",
+  "LEU",
+  "LYS",
+  "MET",
+  "PHE",
+  "PRO",
+  "SER",
+  "THR",
+  "TRP",
+  "TYR",
+  "VAL"
+)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(# Application title
-    titlePanel("ShinyRam"),
-    tags$head(
-        tags$script(src = "https://cdn.rawgit.com/arose/ngl/v0.10.4-1/dist/ngl.js"),
-        tags$script(src="https://cdn.plot.ly/plotly-2.14.0.min.js")
-    ),
-    # Sidebar with a slider input for number of bins
-    fluidPage(fluidRow(
-        column(
-            4,
-            textInput(
-                inputId = "PDB",
-                label = "PDB-code:",
-                value = "1BBB"
-            ),
-            submitButton(),
-            hr(),
-            
-              pickerInput("AA",
-                          "Amino acid selection",
-                          c("ALA","ARG","ASN","ASP","CYS","GLU","GLN","GLY","HIS","ILE","LEU","LYS","MET","PHE","PRO","SER","THR","TRP","TYR","VAL"),
-                          multiple = T,options = list(`actions-box` = TRUE),
-                          selected = c("ALA","ARG","ASN","ASP","CYS","GLU","GLN","GLY","HIS","ILE","LEU","LYS","MET","PHE","PRO","SER","THR","TRP","TYR","VAL"))
-            ,
-            hr(),
-            dropdown(
-                list(
-                    colourpicker::colourInput("bg1", "Not allowed region", value = "#f1eef6"),
-                    colourpicker::colourInput("bg2", "Generously allowed regions", value ='#bdc9e1'),
-                    colourpicker::colourInput("bg3", "Allowed regions", value = '#74a9cf'),
-                    colourpicker::colourInput("bg4", "Favoured regions", value = '#0570b0')
-                ),
-                label = "Background density settings"
-            ),
-            hr(),
-            uiOutput("chainColors"),
-            hr(),
-            fluidRow(
-                tags$div(id = "viewport", style = "width:600px; height:600px;"),
-                tags$script("var stage = new NGL.Stage('viewport',{backgroundColor:'white'})"),
-                tags$script("
+ui <- fluidPage(
+  # Application title
+  titlePanel("ShinyRam"),
+  tags$head(
+    tags$script(src = "https://cdn.rawgit.com/arose/ngl/v0.10.4-1/dist/ngl.js"),
+    tags$script(src = "https://cdn.plot.ly/plotly-2.14.0.min.js")
+  ),
+  # Sidebar with a slider input for number of bins
+  fluidPage(fluidRow(
+    column(
+      4,
+      textInput(
+        inputId = "PDB",
+        label = "PDB-code:",
+        value = "1BBB"
+      ),
+      actionButton("submit","Apply Changes",class="btn-primary btn-lg"),
+      hr(),
+      
+      selectInput("background", "Choose background plot", c("General","Glycine","Preproline","Proline")),
+      
+      pickerInput(
+        "AA",
+        "Amino acid selection",
+        allAA,
+        multiple = T,
+        options = list(`actions-box` = TRUE),
+        selected = allAA
+      )
+      ,
+      hr(),
+      dropdown(
+        list(
+          colourpicker::colourInput("bg1", "Not allowed region", value = "#f1eef6"),
+          colourpicker::colourInput("bg2", "Generously allowed regions", value =
+                                      '#bdc9e1'),
+          colourpicker::colourInput("bg3", "Allowed regions", value = '#74a9cf'),
+          colourpicker::colourInput("bg4", "Favoured regions", value = '#0570b0')
+        ),
+        label = "Background density settings"
+      ),
+      hr(),
+      uiOutput("chainColors"),
+      hr(),
+      
+      fluidRow(
+        tags$div(id = "viewport", style = "width:600px; height:600px;"),
+        tags$script(
+          "var stage = new NGL.Stage('viewport',{backgroundColor:'white'})"
+        ),
+        tags$script(
+          "
       Shiny.addCustomMessageHandler('updateFig', function(pdbcode) {
         stage.removeAllComponents();
         stage.loadFile('rcsb://'+pdbcode, {defaultRepresentation: true});
       });
-    "),
-                tags$script(src="custom.js")
-                
-            )
-            
-        )
-        ,
-        column(8,
-               # Output panel
-               mainPanel(
-                   #plotlyOutput("ramplot"), type = 6
-                 tags$div(id="plotly",style="height:800px;width:891px")
-                 
-               ))
-    )))
+    "
+        ),
+        tags$script(src = "custom.js")
+        
+      )
+      
+    )
+    ,
+    column(8,
+           # Output panel
+           mainPanel(
+             #plotlyOutput("ramplot"), type = 6
+             tags$div(id = "plotly", style = "height:800px;width:891px")
+
+             
+           ))
+  ))
+)
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  output$dummy <- reactive(FALSE)
+  outputOptions(output, "dummy", suspendWhenHidden = FALSE)
+  #session$onSessionEnded(stopApp)
+  options(warn = -1)
+  session$userData$previousPDB<-""
+  session$userData$background<-""
+  #reactive(bio3d::write.pdb(pdb = pdb(), file = paste0(accPDB(), '.pdb')))
   
-    #session$onSessionEnded(stopApp)
-    options(warn = -1)
-    accPDB <- reactive(input$PDB)
-    pdb <- reactive(bio3d::read.pdb(accPDB()))
-    
-    #reactive(bio3d::write.pdb(pdb = pdb(), file = paste0(accPDB(), '.pdb')))
-    
-    tor <- reactive({
-        x <<- torsion.pdb(pdb())
-        tortab <- x[["tbl"]][,c("phi","psi")]
-        spltor <-
-            strsplit(rownames(tortab), split = ".", fixed = T)
-        x <- cbind(tortab,as.data.frame(do.call(rbind,spltor)))
-        x<-rename(x,c('V1'="resi","V2"="chain","V3"="resn"))
-        x
-    })
-    
-    torsubset <- reactive({
-      sub <- subset(tor(),resn %in% input$AA)
-      session$sendCustomMessage("process", list(df=sub,matrix=densMatgeneral,pdb=accPDB()))
-    })
-    
-    output$chainColors <- renderUI({
-        x <- vector('list', length(unique(torsubset()$chain)))
-        c <- 1
-        for (i in unique(torsubset()$chain)) {
-            x[[i]] <-
-                list(colourpicker::colourInput(
+  searchlimit <- function(matrix,percentage, x=1 ) {
+    totalGen <- 100 * sum(matrix$z[matrix$z > x]) / sum(matrix$z)
+    error <- abs(totalGen - percentage)
+    if (error < 0.1) {
+      return(x)
+    }
+    else {
+      if (totalGen > percentage) {
+        searchlimit(matrix,percentage, x = x + x/2)
+      }
+      else {
+        searchlimit(matrix,percentage, x = x - x/2)
+      }
+    }
+  }
+  
+  output$dummy <- reactive({
+    input$submit
+    withProgress(message = 'Making plot', value = 0, {
+      isolate({
+        accPDB <- input$PDB
+        if(session$userData$previousPDB!=accPDB){
+          incProgress(1 / 4, detail = paste("Fetching sequence"))
+          pdb <- bio3d::read.pdb(accPDB)
+          
+          incProgress(1 / 4, detail = paste("Transforming data"))
+          torsion <- torsion.pdb(pdb)
+          print("step 1")
+          tortab <- torsion[["tbl"]][, c("phi", "psi")]
+          print("step 2")
+          spltor <- strsplit(rownames(tortab), split = ".", fixed = T)
+          print("step 3")
+          torsion <- cbind(tortab, as.data.frame(do.call(rbind, spltor)))
+          print("step 4")
+          session$userData$torsion <-
+            rename(torsion, c(
+              'V1' = "resi",
+              "V2" = "chain",
+              "V3" = "resn"
+            ))
+          output$chainColors <- renderUI({
+            isolate({
+              x <- vector('list', length(unique(torsionsubset$chain)))
+              c <- 1
+              for (i in unique(torsionsubset$chain)) {
+                x[[i]] <-
+                  list(colourpicker::colourInput(
                     paste0("chain", i),
                     label = paste("Chain", i),
-                    value = colorSet[c]
-                ))
-            c <- c + 1
+                    value = colorSet[((c-1)%%length(colorSet))+1]
+                  ))
+                c <- c + 1
+              }
+              
+              dropdown(x, label = "Chain color settings")
+            })
+          })
+          session$userData$previousPDB<-accPDB
+          incProgress(1 / 4, detail = paste("Filter data"))
+          
         }
-        dropdown(x, label = "Chain color settings")
-    })
-    
-    output$ramplot <- renderPlotly({
-      
-      
+        else {
+          incProgress(3 / 4, detail = paste("Filter data"))
+        }
+        AAselection<-input$AA
+        if (session$userData$background != input$background){
+          session$userData$background <- input$background 
+          updatePickerInput(session, "AA", selected = 
+          AAselection<-allAA)}
+
+        if (input$background == "General") {
+          matrix <- densMatGeneral
+          torsionsubset <- session$userData$torsion
+          torsionsubset <- subset(torsionsubset, resn %in% AAselection)
+          
+        } else if (input$background == "Glycine") {
+          matrix <- densMatGly
+          # update input$AA to only include glycine
+          updatePickerInput(session, "AA", selected = c("GLY")) 
+          torsionsubset <- subset(session$userData$torsion, resn =="GLY")
+        } else if (input$background == "Preproline") {
+          matrix <- densMatprePro
+          # get a subset of only those amino acids that precede a proline
+          torsionsubset <- data.frame()
+          for (i in 1:length(session$userData$torsion$resn)-1) {
+            if (session$userData$torsion$resn[i+1] == "PRO") {
+              torsionsubset <- rbind(torsionsubset, session$userData$torsion[i,])
+            }
+          }
+          torsionsubset <- subset(torsionsubset, resn %in% AAselection)
+          
+        } else if (input$background == "Proline") {
+          matrix <- densMatPro
+          updatePickerInput(session, "AA", selected = c("PRO")) 
+          torsionsubset <- subset(session$userData$torsion, resn =="PRO")
+        }
+        incProgress(1 / 4, detail = paste("Creating plot"))
         session$sendCustomMessage("updateFig", input$PDB)
-        densMat <- densMatgeneral
-        fig <-
-            plot_ly(
-                x = densMat$x,
-                y = densMat$y,
-                z = t(densMat$z),
-                type = "contour",
-                colorscale = "hot",
-                showscale = F,
-                showlegend = F,
-                hoverinfo = "none",
-                contours = list(
-                    type = "constraint",
-                    operation = "<",
-                    value = 0.00000004293,
-                    showlines = F
-                ),
-                fillcolor = input$bg2
-            ) %>% add_contour(
-                z = t(densMat$z),
-                contours = list(
-                    type = "constraint",
-                    operation = "<",
-                    value = 0.0000012354
-                ),
-                fillcolor = input$bg3,
-                opacity = 0.8
-            ) %>% add_contour(
-                z = t(densMat$z),
-                contours = list(
-                    type = "constraint",
-                    operation = "<",
-                    value = 0.00001265
-                ),
-                fillcolor = input$bg4,
-                opacity = 0.8
-            ) %>% add_contour(
-                z = t(densMat$z),
-                contours = list(
-                    type = "constraint",
-                    operation = ">",
-                    value = 0.00000004293
-                ),
-                fillcolor = input$bg1,
-                opacity = 0.8
-            ) 
-        counter <- 1
-        t <- torsubset()
-        
-        for (i in unique(t$chain)) {
-            x <- input[[paste0("chain", i)]]
-            if (!is.null(x)) {
-                c = input[[paste0("chain", i)]]
-            }
-            else{
-                c = colorSet[counter]
-            }
-            fig <- add_trace(
-                p = fig,
-                x = t$phi[which(t$chain == i)],
-                y = t$psi[which(t$chain == i)],
-                type = "scatter",
-                mode = "markers",
-                showlegend = T,
-                marker = list(color = c),
-                name = i,
-                text = paste0(
-                    i,
-                    ": ",
-                    t$resi[which(t$chain == i)],
-                    " ",
-                    t$resn[which(t$chain == i)],
-                    ": (",
-                    round(t$phi[which(t$chain == i)], 2),
-                    ",",
-                    round(t$psi[which(t$chain == i)], 2),
-                    ")"
-                ),
-                hoverinfo = 'text'
-            )
-
-            counter <- counter + 1
-
+        #get input chain colors from ui
+        chainColors <- vector('list', length(unique(torsionsubset$chain)))
+        for (i in unique(torsionsubset$chain)) {
+          chainColors[[i]] <- input[[paste0("chain", i)]]
         }
 
-        fig <- fig %>% layout(
-            autosize = F,
-            width = 891,
-            height = 800,
-            legend = list(x = 1, y = 0.5),
-            title = accPDB(),
-            xaxis = list(title = "Phi \U03D5 (degrees)"),
-            yaxis = list(title = "Psi ψ (degrees)"),
-            annotations = list(
-                text = 'Made using shinyRam',
-                font = list(size = 12),
-                showarrow = FALSE,
-                xref = 'paper',
-                x = 1,
-                yref = 'paper',
-                y = -0.065
-            )
+        session$sendCustomMessage(
+          "process",
+          list(
+            df = torsionsubset,
+            matrix = matrix,
+            pdb = accPDB,
+            backgroundColors = c(input$bg1, input$bg2, input$bg3, input$bg4),
+            # get the colors from the chain ui color settings and add them to a vector
+            chainColors = unlist(lapply(unique(torsionsubset$chain), function(x) {
+              input[[paste0("chain", x)]]
+            })),
+            limits=c(searchlimit(matrix,85),
+                     searchlimit(matrix,98),
+                     searchlimit(matrix,99.95)
+                     )
+            
+          )
         )
-        fig %>% toWebGL() %>% partial_bundle()
+        #use output to update figures
+        #!ouput$dummy
+      })
     })
-    
-    
-    
-    
+  })
+  
 }
 
 
--- shinyRam/www/custom.js
+++ shinyRam/www/custom.js
@@ -1,56 +1,6 @@
-/* plot_ly(
-    x = densMat$x,
-    y = densMat$y,
-    z = t(densMat$z),
-    type = "contour",
-    colorscale = "hot",
-    showscale = F,
-    showlegend = F,
-    hoverinfo = "none",
-    contours = list(
-        type = "constraint",
-        operation = "<",
-        value = 0.00000004293,
-        showlines = F
-    ),
-    fillcolor = input$bg2
-) %>% add_contour(
-    z = t(densMat$z),
-    contours = list(
-        type = "constraint",
-        operation = "<",
-        value = 0.0000012354
-    ),
-    fillcolor = input$bg3,
-    opacity = 0.8
-) %>% add_contour(
-    z = t(densMat$z),
-    contours = list(
-        type = "constraint",
-        operation = "<",
-        value = 0.00001265
-    ),
-    fillcolor = input$bg4,
-    opacity = 0.8
-) %>% add_contour(
-    z = t(densMat$z),
-    contours = list(
-        type = "constraint",
-        operation = ">",
-        value = 0.00000004293
-    ),
-    fillcolor = input$bg1,
-    opacity = 0.8
-) */
-//colourpicker::colourInput("bg1", "Not allowed region", value = "#f1eef6"),
-//colourpicker::colourInput("bg2", "Generously allowed regions", value ='#bdc9e1'),
-//colourpicker::colourInput("bg3", "Allowed regions", value = '#74a9cf'),
-//colourpicker::colourInput("bg4", "Favoured regions", value = '#0570b0')
-
-// We are transforming the R code to js
-
 colors = ["#7FC97F", "#BEAED4", "#FDC086", "#FFFF99", "#386CB0", "#F0027F", "#BF5B17", "#666666"];
 function mapHextoRGB(hex) {
+    //console.log(hex);
     var bigint = parseInt(hex.replace("#", ""), 16);
     var r = (bigint >> 16) & 255;
     var g = (bigint >> 8) & 255;
@@ -60,7 +10,6 @@ function mapHextoRGB(hex) {
 for (color in colors) {
     colors[color] = mapHextoRGB(colors[color]);
 }
-console.log(colors);
 
 //console.log('loaded!');
 Shiny.addCustomMessageHandler("process",
@@ -72,16 +21,25 @@ Shiny.addCustomMessageHandler("process",
         df = obj['df'];
         matrix = obj['matrix'];
         //transpose matrix.z
-        matrix.z = matrix.z[0].map((_, colIndex) => matrix.z.map(row => row[colIndex]));
+        //matrix.z = matrix.z[0].map((_, colIndex) => matrix.z.map(row => row[colIndex]));
 
         pdb = obj['pdb'];
+        backgroundColors = obj['backgroundColors'];
+        chainColors = obj['chainColors'];
+        limits = obj['limits'];
+        // if chainColors is a single value, make it a list
+        if (typeof chainColors === 'string' || chainColors instanceof String) {
+            chainColors = [chainColors];
+        }
+
+        console.log(chainColors);
         //Transform df from format {chain:[],resi:[],resn[],phi:[],psi:[]} to format {[chain,resi,resn,phi,psi]}
         df["processed"] = df.chain.map(function (d, i) { return [d, df.resi[i], df.resn[i], df.phi[i], df.psi[i]] });
 
 
 
         //console.log("TESTER: " + TESTER);
-        console.log("df: " + JSON.stringify(df));
+        //console.log("df: " + JSON.stringify(df));
         //console.log("matrix: " + matrix["z"]);
         //df is an object containing "chain", "phi", "psi","resi" and "resn"
         // we want to plot the data in the plotly div
@@ -95,7 +53,7 @@ Shiny.addCustomMessageHandler("process",
 
         // contour plot
         // make contours like this:
-
+        console.log(limits)
         var data = [
             // plot the matrix as contours
             {
@@ -105,13 +63,13 @@ Shiny.addCustomMessageHandler("process",
                 type: 'contour',
                 showscale: false,
                 showlegend: false,
-                hoverinfo: 'none',
+                //hoverinfo: 'none',
                 contours: {
                     type: 'constraint',
                     operation: '<',
-                    value: 0.00000004293,
+                    value: limits[2],
                 },
-                fillcolor: mapHextoRGB("#bdc9e1")
+                fillcolor: mapHextoRGB(backgroundColors[1])
                 // colorscale borders are [0.00000004293, 0.0000012354, 0.00001265]
                 //colors should be #f1eef6, #bdc9e1, #74a9cf, #0570b0
             },
@@ -121,12 +79,13 @@ Shiny.addCustomMessageHandler("process",
                 z: matrix['z'],
                 type: 'contour',
                 showlegend: false,
+                //hoverinfo: 'none',
                 contours: {
                     type: 'constraint',
                     operation: '<',
-                    value: 0.0000012354,
+                    value: limits[1],
                 },
-                fillcolor: mapHextoRGB("#74a9cf"),
+                fillcolor: mapHextoRGB(backgroundColors[2]),
                 opacity: 0.8
 
             }
@@ -137,12 +96,13 @@ Shiny.addCustomMessageHandler("process",
                 z: matrix['z'],
                 type: 'contour',
                 showlegend: false,
+                hoverinfo: 'none',
                 contours: {
                     type: 'constraint',
                     operation: '<',
-                    value: 0.00001265,
+                    value: limits[0],
                 },
-                fillcolor: mapHextoRGB("#0570b0"),
+                fillcolor: mapHextoRGB(backgroundColors[3]),
                 opacity: 0.8
             },
             {
@@ -151,19 +111,23 @@ Shiny.addCustomMessageHandler("process",
                 z: matrix['z'],
                 type: 'contour',
                 showlegend: false,
+                hoverinfo: 'none',
                 contours: {
                     type: 'constraint',
                     operation: '>',
-                    value: 0.00000004293,
+                    value: limits[2],
                 },
-                fillcolor: mapHextoRGB("#f1eef6"),
+                fillcolor: mapHextoRGB(backgroundColors[0]),
                 opacity: 0.8
             }
         ];
+
+
         //markers per chain
         for (var i = 0; i < uniqueChains.length; i++) {
             var chain = uniqueChains[i];
             var chainData = df.processed.filter(function (x) { return x[0] == chain; });
+
             var chainData = {
                 x: chainData.map(function (x) { return x[3]; }),
                 y: chainData.map(function (x) { return x[4]; }),
@@ -172,7 +136,9 @@ Shiny.addCustomMessageHandler("process",
                 type: 'scatter',
                 name: chain,
                 marker: {
-                    color: colors[i % colors.length],
+                    // if chainColors is null or undefined, use the default colors, this is necessary for first loading as well as when another pdb is loaded, since UI only updates after everything is done.
+                    // This was a pain to figure out.
+                    color: chainColors != null && chainColors[i] ? mapHextoRGB(chainColors[i]) : colors[i % colors.length],
                     size: 12
                 },
                 text: chainData.map(function (x) { return x[0] + ": " + x[1] + " " + x[2] + ": (" + x[3] + ", " + x[4] + ")"; })
